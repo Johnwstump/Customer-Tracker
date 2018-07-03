01ff343493585dff81baf12a9110addb993271ed
Johnwstump <johnwstump@outlook.com>
2018-07-02 23:40:03 -0400

    Converted from XML to Java Configuration
    Translated XML into Spring MVC and Hibernate Java configuration files.

---
+++ src/com/johnwstump/springdemo/config/HibernateConfig.java
@@ -0,0 +1,74 @@
+package com.johnwstump.springdemo.config;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		ComboPooledDataSource source = new ComboPooledDataSource();
+		try {
+			source.setDriverClass("com.mysql.cj.jdbc.Driver");
+			//TODO Replace with Environment Variables
+			source.setJdbcUrl("jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false");
+			source.setUser("springstudent");
+			source.setPassword("springstudent");
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+
+		/* Connection Pool properties */
+		source.setInitialPoolSize(5);
+		source.setMinPoolSize(5);
+		source.setMaxPoolSize(20);
+		source.setMaxIdleTime(30000);
+		return source;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("com.johnwstump.springdemo.entity");
+		sessionFactory.setHibernateProperties(getHibernateProperties());
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) throws Exception{
+		HibernateTransactionManager manager = new HibernateTransactionManager();
+		manager.setSessionFactory(sessionFactory);
+		return manager;
+	}
+	
+	private Properties getHibernateProperties() {
+		Properties hibernateProperties = new Properties();
+		
+		hibernateProperties.put("hibernate.temp.use_jdbc_metadata_defaults","false");
+		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		hibernateProperties.put("hibernate.show_sql", true);
+		
+		return hibernateProperties;
+	}
+	
+
+}
---
+++ src/com/johnwstump/springdemo/config/SpringMVCConfig.java
@@ -0,0 +1,38 @@
+package com.johnwstump.springdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.johnwstump")
+@EnableAspectJAutoProxy
+public class SpringMVCConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+}
