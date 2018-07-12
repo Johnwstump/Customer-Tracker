3bef0c192386fd42d7c5fed6cf2ccf7df31cc5a0
Johnwstump <johnwstump@outlook.com>
2018-07-12 11:16:49 -0400

    Added REST API Create for Customer entity

--- src/com/johnwstump/springdemo/rest/CustomerRestController.java
+++ src/com/johnwstump/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,14 @@ public class CustomerRestController {
 		if (customer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
+		return customer;
+	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		// Set id to Empty value to force Hibernate save instead of update
+		customer.setId(0);
+		customerService.saveCustomer(customer);
 		return customer;
 	}
 	
